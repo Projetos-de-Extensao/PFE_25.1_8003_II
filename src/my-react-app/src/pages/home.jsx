55f716a6c2ff5e59b4e4b27c0274f1781cc6d44a
Roberto Ramos Pereira <202408320931@alunos.ibmec.edu.br>
2025-06-02 22:51:19 -0300

    Update Newsletter

--- src/my-react-app/src/pages/home.jsx
+++ src/my-react-app/src/pages/home.jsx
@@ -2,6 +2,7 @@
 import Header from '../components/Header/Header'; 
 import Footer from '../components/Footer/Footer';
 import Menu from '../components/Menu/Menu';
+import Newsletter from '../components/Newsletter/Newsletter';
 
 
 
@@ -10,7 +11,7 @@ function Home() {
     <>
       <Header />
       <Menu/>
-      
+      <Newsletter/>
       <Footer />
     </>
   );
