0b7516cbb2827376a2647b70be739e3ed3c85842
Roberto Ramos Pereira <202408320931@alunos.ibmec.edu.br>
2025-06-03 00:11:43 -0300

    update

--- src/my-react-app/src/pages/home.jsx
+++ src/my-react-app/src/pages/home.jsx
@@ -1,30 +1,20 @@
-
 import Header from '../components/Header/Header'; 
 import Footer from '../components/Footer/Footer';
 import Menu from '../components/Menu/Menu';
 import Newsletter from '../components/Newsletter/Newsletter';
 import DepoimentosSection from '../components/DepoimentosSection/DepoimentosSection';
-<<<<<<< HEAD
 import NossosServicos from '../components/NossosServicos/NossosServicos';
-=======
 import Carrossel from '../components/Carrossel/Carrossel';
->>>>>>> 80b75ca7c2b60ce631144eb778d489fed905e1c2
-
 
 function Home() {
   return (
     <>
       <Header />
-      <Menu/>
-<<<<<<< HEAD
-       <EventCard/>
-      <NossosServicos />
-=======
+      <Menu />
       <Carrossel />
-       
->>>>>>> 80b75ca7c2b60ce631144eb778d489fed905e1c2
-      <DepoimentosSection/>
-      <Newsletter/>
+      <NossosServicos />
+      <DepoimentosSection />
+      <Newsletter />
       <Footer />
     </>
   );
